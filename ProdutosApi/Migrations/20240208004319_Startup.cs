2678e4853ae91603c3555bd8ad80679f41a28ad7
Ricardo Gomes Fontana Filho <ricardogomesfontana@gmail.com>
2024-02-09 01:44:00 -0300

    feat/produtos-api/autoglass | Migrations

---
+++ ProdutosApi/Migrations/20240208004319_Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProdutosApi.Migrations
+{
+    public partial class Startup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fornecedores",
+                columns: table => new
+                {
+                    CodigoFornecedor = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DescricaoFornecedor = table.Column<string>(type: "TEXT", nullable: true),
+                    CNPJ = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedores", x => x.CodigoFornecedor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    CodigoProduto = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DescricaoProduto = table.Column<string>(type: "TEXT", nullable: true),
+                    SituacaoProduto = table.Column<bool>(type: "INTEGER", nullable: false),
+                    DataFabricacao = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataValidade = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    FornecedorCodigoFornecedor = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.CodigoProduto);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Fornecedores_FornecedorCodigoFornecedor",
+                        column: x => x.FornecedorCodigoFornecedor,
+                        principalTable: "Fornecedores",
+                        principalColumn: "CodigoFornecedor",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_FornecedorCodigoFornecedor",
+                table: "Produtos",
+                column: "FornecedorCodigoFornecedor");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Fornecedores");
+        }
+    }
+}
---
+++ ProdutosApi/Migrations/20240208025120_ConfigureForeignKey.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProdutosApi.Migrations
+{
+    public partial class ConfigureForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Fornecedores_FornecedorCodigoFornecedor",
+                table: "Produtos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Produtos_FornecedorCodigoFornecedor",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "FornecedorCodigoFornecedor",
+                table: "Produtos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CodigoFornecedor",
+                table: "Produtos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CodigoFornecedor",
+                table: "Produtos",
+                column: "CodigoFornecedor");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Fornecedores_CodigoFornecedor",
+                table: "Produtos",
+                column: "CodigoFornecedor",
+                principalTable: "Fornecedores",
+                principalColumn: "CodigoFornecedor",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Fornecedores_CodigoFornecedor",
+                table: "Produtos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Produtos_CodigoFornecedor",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "CodigoFornecedor",
+                table: "Produtos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "FornecedorCodigoFornecedor",
+                table: "Produtos",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_FornecedorCodigoFornecedor",
+                table: "Produtos",
+                column: "FornecedorCodigoFornecedor");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Fornecedores_FornecedorCodigoFornecedor",
+                table: "Produtos",
+                column: "FornecedorCodigoFornecedor",
+                principalTable: "Fornecedores",
+                principalColumn: "CodigoFornecedor",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
