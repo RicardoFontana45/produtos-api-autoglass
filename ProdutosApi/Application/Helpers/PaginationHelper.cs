19fd0ab342614b2aa55ae1fa70ba14ae5c0f1dec
Ricardo Gomes Fontana Filho <ricardo.filho@inflor.com.br>
2024-02-09 01:42:48 -0300

    feat/produtos-api/autoglass | Helpers

---
+++ ProdutosApi/Application/Helpers/PaginationHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ProdutosApi.Application.Helpers
+{
+    public class PaginationHelper<T>
+    {
+        public int TotalItems { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+}
