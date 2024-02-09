651270ea76af70637c18b66624184a4878c29c6c
Ricardo Gomes Fontana Filho <ricardo.filho@inflor.com.br>
2024-02-09 01:41:22 -0300

    feat/produtos-api/autoglass | Controllers

---
+++ ProdutosApi/API/Controllers/FonecedorController.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProdutosApi.API.DTOs;
+using ProdutosApi.Application.Commands;
+using ProdutosApi.Application.Helpers;
+using ProdutosApi.Application.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProdutosApi.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class FonecedorController : ControllerBase
+    {
+        [HttpPost]
+        public Task<FornecedorDTO> CriarFornecedorAsync([FromBody] CriarFornecedorCommand command, [FromServices] IMediator mediator) => mediator.Send(command);
+
+        [HttpPut("id")]
+        public Task<FornecedorDTO> EditarFOrnecedorAsync(int id, [FromBody] EditarFornecedorCommand command, [FromServices] IMediator mediator)
+        {
+            command.CodigoFornecedor = id;
+            return mediator.Send(command);
+        }
+
+        [HttpGet("id")]
+        public Task<FornecedorDTO> GetFornecedorByIdAsync(int id, [FromServices] IFornecedorQueries queries) => queries.GetFornecedorById(id);
+
+
+        [HttpGet]
+        public Task<PaginationHelper<FornecedorDTO>> GetFornecedoresAsync([FromServices] IFornecedorQueries queries,
+                int pagina = 1,
+                int tamanhoPagina = 10,
+                string descricaoFornecedor = null,
+                string CNPJ = null
+            ) => queries.GetAllFornecedores(pagina, tamanhoPagina, descricaoFornecedor, CNPJ);
+    }
+}
---
+++ ProdutosApi/API/Controllers/ProdutoController.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProdutosApi.API.DTOs;
+using ProdutosApi.Application.Commands;
+using ProdutosApi.Application.Helpers;
+using ProdutosApi.Application.Queries;
+using System;
+using System.Threading.Tasks;
+
+namespace ProdutosApi.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class ProdutoController : ControllerBase
+    {
+        [HttpPost]
+        public Task<ProdutoDTO> CriarProdutoAsync([FromBody] CriarProdutoCommand command, [FromServices] IMediator mediator) => mediator.Send(command);
+
+
+        [HttpPut("id")]
+        public Task<ProdutoDTO> EditarProdutoAsync([FromBody] EditarProdutoCommand command, [FromServices] IMediator mediator, int id) {
+            command.CodigoProduto = id;
+            return mediator.Send(command);
+        }
+
+        [HttpDelete("id")]
+        public Task<ProdutoDTO> DeleteProdutoAsync([FromServices] IMediator mediator, int id)
+        {
+            var command = new DeletarProdutoCommand() { CodigoProduto = id };
+            return mediator.Send(command);
+        }
+
+        [HttpGet]
+        public Task<PaginationHelper<ProdutoDTO>> GetAllProdutosAsync(
+            [FromServices] IProdutoQueries query,
+            [FromQuery] int pagina = 1,
+            [FromQuery] int tamanhoPagina = 10,
+            [FromQuery] DateTime? dataValidadeMin = null,
+            [FromQuery] DateTime? dataValidadeMax = null,
+            [FromQuery] DateTime? dataFabricacaoMin = null,
+            [FromQuery] DateTime? dataFabricacaoMax = null,
+            [FromQuery] int? codigoFornecedor = null) => query.GetAllProdutos(pagina, tamanhoPagina, dataValidadeMin, dataValidadeMax, dataFabricacaoMin, dataFabricacaoMax, codigoFornecedor);
+
+        [HttpGet("id")]
+        public Task<ProdutoDTO> GetAsync(int id, [FromServices] IProdutoQueries query) => query.GetProdutoById(id);
+    }
+}
