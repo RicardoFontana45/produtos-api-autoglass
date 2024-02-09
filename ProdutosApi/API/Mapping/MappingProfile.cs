28b208ba56abf077cd842020cc1cf6c609bdb2f8
Ricardo Gomes Fontana Filho <ricardo.filho@inflor.com.br>
2024-02-09 01:41:49 -0300

    feat/produtos-api/autoglass | AutoMapper

---
+++ ProdutosApi/API/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ProdutosApi.API.DTOs;
+using ProdutosApi.Application.Commands;
+using ProdutosApi.Domain.Entitites;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Produto, ProdutoDTO>();
+        CreateMap<ProdutoDTO, Produto>();
+        CreateMap<CriarProdutoCommand, Produto>();
+        CreateMap<EditarProdutoCommand, Produto>();
+
+        CreateMap<Fornecedor, FornecedorDTO>();
+        CreateMap<FornecedorDTO, Fornecedor>();
+        CreateMap<CriarFornecedorCommand, Fornecedor>();
+        CreateMap<EditarFornecedorCommand, Fornecedor>();
+    }
+}
