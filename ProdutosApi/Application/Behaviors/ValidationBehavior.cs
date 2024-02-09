17907e85140a0345e34d1629210f580df70d8353
Ricardo Gomes Fontana Filho <ricardo.filho@inflor.com.br>
2024-02-09 01:42:02 -0300

    feat/produtos-api/autoglass | MediatR Pipeline

---
+++ ProdutosApi/Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+using ProdutosApi.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProdutosApi.Application.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>, ICommand
+
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var validationResult = request.Validate();
+
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException("Ops! Algo deu errado!", validationResult.Errors);
+            }
+
+            return await next();
+
+        }
+    }
+}
