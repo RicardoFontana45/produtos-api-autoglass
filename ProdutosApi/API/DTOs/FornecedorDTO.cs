cf39bb63c03e32bc4a78cf0bc05054ba0dbb36f1
Ricardo Gomes Fontana Filho <ricardo.filho@inflor.com.br>
2024-02-09 01:41:38 -0300

    feat/produtos-api/autoglass | DTOs

---
+++ ProdutosApi/API/DTOs/FornecedorDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ProdutosApi.API.DTOs
+{
+    public class FornecedorDTO
+    {
+        public int CodigoFornecedor { get; set; }
+        public string DescricaoFornecedor { get; set; }
+        public string CNPJ { get; set; }
+        public IEnumerable<ProdutoDTO> Produtos { get; set; }
+    }
+}
---
+++ ProdutosApi/API/DTOs/ProdutoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProdutosApi.API.DTOs
+{
+    public class ProdutoDTO
+    {
+        public int CodigoProduto { get; set; }
+        public string DescricaoProduto { get; set; }
+        public bool SituacaoProduto { get; set; }
+        public DateTime DataFabricacao { get; set; }
+        public DateTime DataValidade { get; set; }
+        public int CodigoFornecedor { get; set; }
+    }
+}
